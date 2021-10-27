318d6b0f8d0a07b542f17a75c503e47ab34168de
Mukhammad Ilham Firdaus <90994680+NocIcoN@users.noreply.github.com>
2021-10-27 22:24:48 +0700

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,44 +1,44 @@
 package MukhammadIlhamFirdaus;
-
+//21.51.0008
 import java.util.Scanner;
 
 public class Palindrome {
-    // if you put word with "ng" or "ny" that's should not be changed to "gn" or "syn"
+    // if you put word containt "ng" or "ny" that's shouldn't changed to "gn" or "yn"
     public static void main(String[] args) {
-        String awal;
-        String balik = "";
-        String cek = "";
-        Scanner kata = new Scanner(System.in);
-        System.out.println("Masukan kata atau kalimat yang akan dibalik : ");
-        awal = kata.nextLine();
-        for (int i = awal.length() - 1; i >= 0; i--) {
-            char c = awal.charAt(i);
-            cek = String.valueOf(c);
-            if (cek.equalsIgnoreCase("g")){
-                char c1 = awal.charAt(i-1);
-                String cek1 = String.valueOf(c1);
-                if (cek1.equalsIgnoreCase("n")){
-                    balik += awal.charAt(i -1);
-                    i -= 1;
+        String input;
+        String output = "";
+        String set = "";
+        Scanner word = new Scanner(System.in);
+        System.out.println("Put Your Word or Sentences Here");
+        input = word.nextLine();
+        for (int y = input.length() - 1; y >= 0; y--) {
+            char x = input.charAt(y);
+            set = String.valueOf(x);
+            if (set.equalsIgnoreCase("g")){
+                char c1 = input.charAt(y-1);
+                String set1 = String.valueOf(c1);
+                if (set1.equalsIgnoreCase("n")){
+                    output += input.charAt(y -1);
+                    y -= 1;
                 }
             }
-            if (cek.equalsIgnoreCase("y")){
-                char c1 = awal.charAt(i-1);
-                String cek1 = String.valueOf(c1);
-                if (cek1.equalsIgnoreCase("n")){
-                    balik += awal.charAt(i -1);
-                    i -= 1;
+            if (set.equalsIgnoreCase("y")){
+                char c1 = input.charAt(y-1);
+                String set1 = String.valueOf(c1);
+                if (set1.equalsIgnoreCase("n")){
+                    output += input.charAt(y -1);
+                    y -= 1;
                 }
             }
-            balik += c;
+            output += x;
         }
 
-        System.out.println("Hasil kata yang dibalik :" + balik);
+        System.out.println("The Value " + output);
 
-        for (int i = awal.length() - 1; i >= 0; i--) {
-            char c = awal.charAt(i);
-            cek += String.valueOf(c);
+        for (int y = input.length() - 1; y >= 0; y--) {
+            char x = input.charAt(y);
+            set += String.valueOf(x);
         }
-        System.out.println("MISSION COMPLEATED");
+        System.out.println("Mission Succes");
     }
 }
